2e5b5c02b78797f0884c25b9564f134fe197e130
drleowang <106391786+drleowang@users.noreply.github.com>
2023-10-18 17:46:12 -0400

    Create vpc.tf

---
+++ terraform/vpc.tf
@@ -0,0 +1,54 @@
+# Specify the provider and region
+provider "aws" {
+  region = "us-east-1" # Change to your desired region
+}
+
+# Create a VPC
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16" # You can adjust the CIDR block as needed
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "my-vpc"
+  }
+}
+
+# Create an internet gateway
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+  tags = {
+    Name = "my-igw"
+  }
+}
+
+# Create a route table
+resource "aws_route_table" "my_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+  tags = {
+    Name = "my-route-table"
+  }
+}
+
+# Create a route to the internet via the internet gateway
+resource "aws_route" "my_route" {
+  route_table_id = aws_route_table.my_route_table.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.my_igw.id
+}
+
+# Create a subnet
+resource "aws_subnet" "my_subnet" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.0.0/24" # Adjust the CIDR block for your subnet
+  availability_zone = "us-east-1a" # Change the availability zone as needed
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "my-subnet"
+  }
+}
+
+# Associate the route table with the subnet
+resource "aws_route_table_association" "my_subnet_association" {
+  subnet_id      = aws_subnet.my_subnet.id
+  route_table_id = aws_route_table.my_route_table.id
+}
