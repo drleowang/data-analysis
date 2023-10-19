026fb0f0cca0200d00593ea7abef0d1072565a5f
drleowang <leo.wang.dr@gmail.com>
2023-10-19 13:16:41 -0400

    aws

--- terraform/aws/variables.tf
+++ terraform/aws/variables.tf
@@ -1,13 +1,13 @@
-# variable "AWS_ACCESS_KEY_ID" {
-#   description = "AWS access key"
-#   type        = string
-#   sensitive    = true
-# #   default = var.TF_VAR_AWS_ACCESS_KEY_ID
-# }
+variable "AWS_ACCESS_KEY_ID" {
+  description = "AWS access key"
+  type        = string
+  sensitive    = true
+  default = ""
+}
 
-# variable "AWS_SECRET_ACCESS_KEY" {
-#   description = "AWS secret key"
-#   type        = string
-#   sensitive    = true
-# #   default = var.TF_VAR_AWS_ACCESS_KEY_ID
-# }
+variable "AWS_SECRET_ACCESS_KEY" {
+  description = "AWS secret key"
+  type        = string
+  sensitive    = true
+  default = ""
+}
