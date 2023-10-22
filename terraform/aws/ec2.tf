7005e0b979cbaeafd63f71360d743ec4ea0fd218
drleowang <leo.wang.dr@gmail.com>
2023-10-22 12:47:57 -0400

    aws ec2 sg

--- terraform/aws/ec2.tf
+++ terraform/aws/ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "my_instance" {
   ami           = "ami-0c55b159cbfafe1f0"  # Ubuntu 20.04 LTS 的 AMI ID
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.my_subnet.id
-  security_groups = [aws_security_group.my_security_group.name]
+  security_groups = [aws_security_group.my_ec2_security_group.name]
 }
 
 resource "aws_security_group" "my_ec2_security_group" {
